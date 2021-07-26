311e584a96fea468204957fc07d52c4ecb21c5bd
ahmedhabiib <info.ahmed.developer@gmail.com>
2021-07-26 23:06:46 +0300

    added link href teacher

--- models/students/student.js
+++ models/students/student.js
@@ -1,45 +1,50 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const studentSchema = new Schema({
-    firstname: {
-        type: String,
-        required: true
-    },
-    stdID: {
-        type: Number,
-        default: 102,
-    },
-    lastname: {
-        type: String,
-        required: true
-    },
-    gender: {
-        type: String,
-        enum: ['male', 'female'],
-        required: true
-    },
-    title: {
-        type: String,
-        default: 'student'
-    },
-    class: {
-        type: Schema.Types.ObjectId,
-        ref: 'Class'
-    },
-    age: {
-        type: Number,
-        required: true
-    },
-    status: {
-        type: Boolean,
-        default: true,
-    },
-    parent: {
-        type: Schema.Types.ObjectId,
-        ref: 'Parent',
-        required: true
-    }
+  firstname: {
+    type: String,
+    required: true,
+  },
+  stdID: {
+    type: Number,
+    default: 102,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female"],
+    required: true,
+  },
+  title: {
+    type: String,
+    default: "student",
+  },
+  class: {
+    type: Schema.Types.ObjectId,
+    ref: "Class",
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: Boolean,
+    default: true,
+  },
+  parent: {
+    type: Schema.Types.ObjectId,
+    ref: "Parent",
+    required: true,
+  },
+  joinedAt: {
+    type: Date,
+    default: new Date(),
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('Student', studentSchema);
+module.exports = mongoose.model("Student", studentSchema);
